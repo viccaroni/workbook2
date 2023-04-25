c98e8a2e2ac14e2e4a0bbc8d6d24fb3dee33861b
Victoria Hernandez <v11jhernandez@gmail.com>
2023-04-25 10:24:33 -0400

    initial js, playing with variables

---
+++ program.js
@@ -0,0 +1,16 @@
+// var firstName;
+// firstName = "Victoria";
+// lastName = "Hernandez";
+// console.log("Hello my name is " + firstName + " " + lastName);
+
+var customerId = 555;
+console.log(customerId);
+var customerFirstName = "Victoria";
+var customerMiddleName = "Jill Angela";
+var customerLastName = "Hernandez";
+console.log(customerFirstName);
+console.log(customerMiddleName);
+console.log(customerLastName);
+var dateOfBirth;
+var gender;
+var autoPolicyNumber;
