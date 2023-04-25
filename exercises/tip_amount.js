5ff0433b320d5b92d5606c051ef7f91e77931a78
Victoria Hernandez <v11jhernandez@gmail.com>
2023-04-25 11:42:26 -0400

    more exercises

--- exercises/tip_amount.js
+++ exercises/tip_amount.js
@@ -1,2 +1,4 @@
-var percentage = 20
-var billAmount 
+var percentage = 0.20;
+var billAmount = 40;
+tipAmount = billAmount * percentage;
+console.log("The tip on a $" + billAmount + " food bill is $" + tipAmount);
