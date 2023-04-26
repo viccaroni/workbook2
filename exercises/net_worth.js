72887b22a651a9b4c6b602f54ad77f7c10a96830
Victoria Hernandez <v11jhernandez@gmail.com>
2023-04-26 08:47:30 -0400

    net worth

--- exercises/net_worth.js
+++ exercises/net_worth.js
@@ -1,4 +1,11 @@
-var assets = 80000;
-var debt = 20000;
-netWorth = assets - debt;
+// var assets = 80000;
+// var debt = 20000;
+// var obj = new Object();
+// obj.assets = 80000;
+// obj.debts = 20000;
+var obj = { assets:80000, debts:20000 }
+
+
+var netWorth = obj.assets - obj.debts;
+
 console.log("The net worth is " + netWorth);
