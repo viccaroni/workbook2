49baa6581fd44d542861d97413ea0509a0e43b43
Victoria Hernandez <v11jhernandez@gmail.com>
2023-04-26 12:02:27 -0400

    doing exercise myself to understand

---
+++ demos/exercises/conversion-tests.js
@@ -0,0 +1,19 @@
+// Description: This script tests various numeric
+// conversion techniques
+
+// Author: Victoria Hernandez
+
+var oneHundred = "101.1";
+var fiftyFive = "55";
+var stevens = "402 Stevens";
+var numberFive = "Number 5";
+
+a = parseInt(oneHundred);
+console.log("The result for " + oneHundred + " is " + a);
+
+b = parseFloat(oneHundred);
+console.log("The result for " + oneHundred + " is " + b)
+
+c = Number("101.1");
+console.log("The result for " + oneHundred + " is " + c);
+
