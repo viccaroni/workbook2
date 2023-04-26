4a0f7c3f12b27c57280f416d00a7f7a95cf67ce8
Victoria Hernandez <v11jhernandez@gmail.com>
2023-04-26 09:55:57 -0400

    parse string example

---
+++ demos/parse-string-to-number.js
@@ -0,0 +1,8 @@
+// declare variable "a" and assigning a string to it
+var a = "100";
+var b = parseInt(a);
+ var dataTypeOfAVariable = typeof(a);
+console.log(typeof(a));
+
+var dataTypeOfBVariable = typeof(b);
+console.log(typeof(b));
