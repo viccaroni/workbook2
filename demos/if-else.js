3d4ae07c7de50bcabf62cc92f88ff7fa4ec7adbc
Victoria Hernandez <v11jhernandez@gmail.com>
2023-04-26 10:49:08 -0400

    if else

---
+++ demos/if-else.js
@@ -0,0 +1,16 @@
+var grade = "A";
+
+if(grade == "A"){
+    console.log("ice cream sundae");
+}
+else{
+    console.log("No sundae for you!");
+}
+
+var testScore = 69;
+
+if (testScore >= 70) {
+    console.log("Pass!");  
+} else {
+    console.log("Fail...");
+}
