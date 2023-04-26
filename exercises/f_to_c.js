a39b989ddfa157798b73f7d3d9ef41ff41ea0729
Victoria Hernandez <v11jhernandez@gmail.com>
2023-04-26 08:47:25 -0400

    f to c

---
+++ exercises/f_to_c.js
@@ -0,0 +1,11 @@
+var farenheit = 100;
+
+var celsius = ((farenheit - 32) * 5) / 9;
+console.log(
+  "turning " +
+    farenheit +
+    " farenheit " +
+    "into celcius is equal to " +
+    Math.round(celsius) +
+    " celsius"
+);
