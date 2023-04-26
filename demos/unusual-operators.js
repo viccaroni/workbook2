39821f82470692859562a2bc5d6412be332bcbb8
Victoria Hernandez <v11jhernandez@gmail.com>
2023-04-26 09:13:31 -0400

    operator demo

---
+++ demos/unusual-operators.js
@@ -0,0 +1,22 @@
+// // declare variable
+// var count;
+// // assign variable
+// count = 0;
+
+// declare and assign variable
+var count = 0;
+// more known and easier and quicker to use
+console.log(count);
+// count = count + 1; 
+count++;
+//basically means to add one to the count
+console.log(count);
+//-- subtracts 
+count--
+console.log(count)
+
+var onHand = 10;
+var qtyRecieved = 6;
+// onHand = onHand + qtyRecieved; is the same as:
+onHand += qtyRecieved;
+console.log(onHand)
