8c0c7efc84515ef0d70e97ccb195b00eed8571bb
Victoria Hernandez <v11jhernandez@gmail.com>
2023-04-26 15:29:35 -0400

    const

---
+++ demos/const.js
@@ -0,0 +1,9 @@
+//const and let is the same as var
+const firstName = "Dana";
+let count = 0;
+let message;
+count++; // valid
+message = "Hello " + firstName; // valid
+console.log(message);
+//cantoverrun a variable thats already taken 
+firstName = "Mark";
